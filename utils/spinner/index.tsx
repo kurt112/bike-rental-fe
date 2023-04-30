86a82f07536db954dc9f9970df728a70ee7ac8ae
Kurt Lupin Orioque <korioque@vertere-gs.com>
2023-04-30 11:01:57 +0800

    change timezone of date

--- utils/spinner/index.tsx
+++ utils/spinner/index.tsx
@@ -2,11 +2,11 @@ import { Spinner } from '@chakra-ui/react'
 
 const Loading = () => {
     return <Spinner
-        thickness='4px'
+        thickness='3px'
         speed='0.65s'
         emptyColor='gray.200'
         color='blue.500'
-        size='xl'
+        size='md'
     />
 }
 
